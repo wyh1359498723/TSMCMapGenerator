6aec667e0cac1e909954643601143283d8498729
wuyuhan <yuhan.wu_sh@ht-tech.com>
2025-08-29 16:37:19 +0800

    2

--- Repository.cs
+++ Repository.cs
@@ -62,5 +62,54 @@ namespace TSMCMapGenerator
                 FLAT = r["FLAT"].ToString()
             }).ToList();
         }
+
+        public List<LotInfoModel> GetLotInfoForMapGeneration()
+        {
+            string sql = @"
+                select 
+                    main.lotid,
+                    main.cp,
+                    main.rp,
+                    main.tester_count,
+                    main.allwfno,
+                    nvl(b.DEVICE, a.product_no) as device,
+                    b.cust_code
+                from 
+                    (
+                        select 
+                            lotid,
+                            cp,
+                            rp,
+                            count(1) as tester_count,
+                            listagg(wfno, ',') within group (order by to_number(wfno)) as allwfno 
+                        from 
+                            wip_lot_testerstate  
+                        where 
+                             step >= 3
+                        group by 
+                            lotid, cp, rp
+                    ) main
+                    left join htmms_get_lotinfo b on main.lotid = b.lot_id
+                    left join wip_lot a on a.order_no || '.' || a.order_sub_lot = b.lot_id
+                where 
+                    exists (
+                        select 1 
+                        from MMS_TSMC_DEVICE mms
+                        where mms.MTD_DEVICE = nvl(b.DEVICE, a.product_no)
+                          and mms.MTD_CUST = b.cust_code and mms.MTD_CUST='MCB' and mms.MTD_DEVICE='MBB201QB'
+                    )";
+
+            var dt = Query(sql);
+            return dt.AsEnumerable().Select(r => new LotInfoModel
+            {
+                LotId = r["LOTID"].ToString(),
+                Cp = r["CP"].ToString(),
+                Rp = r["RP"].ToString(),
+                Tester_Count = Convert.ToInt32(r["TESTER_COUNT"]),
+                AllWfNo = r["ALLWFNO"].ToString(),
+                Device = r["DEVICE"].ToString(),
+                Cust_Code = r["CUST_CODE"].ToString()
+            }).ToList();
+        }
     }
 }
